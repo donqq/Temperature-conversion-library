1f47488944ecc1973e5c74fef715c0ba7210a1ad
Liyanage Dilanga <qqslbd@gmail.com>
2018-10-05 02:47:19 +0530

    Add files via upload

---
+++ SAITM_FC/Class1.vb
@@ -0,0 +1,42 @@
+﻿
+'=====================================================================
+'
+'Developed by Badde Liyanage Don Dilanga. bld@dilanga.com. 
+'SAITM VB.NET Project on 2009 /10/02 
+'
+'=====================================================================
+
+'//Make a library file so you can use this as a reference
+'//two datas should be inserted 
+
+'_value : the value need to be converted
+'_symbol : the identifer
+
+'and you got FtoC (the answer as return)
+
+Public Class SAITM_FC_BLD_Dilanga
+
+    Dim _getValue As Double
+
+    Public Function FtoC(ByVal _value As Double, ByVal _symbol As String) As Double
+        '//symbol is required to identify which procedure should be done
+        '//either F to C or C to F 
+
+        'F is Farenheit 
+        'C is Celcius
+
+        If (_symbol = "CtoF") Then
+
+            _getValue = Val((_value + 160) / 5)
+
+        ElseIf (_symbol = "FtoC") Then
+
+            _getValue = Val((5 * _value) - 160)
+
+        End If
+
+        Return _getValue
+
+    End Function
+
+End Class
---
+++ SAITM_FC/My Project/AssemblyInfo.vb
@@ -0,0 +1,39 @@
+﻿Imports System.Resources
+
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("CSSCal 0.1 Farenheit Celcius")> 
+<Assembly: AssemblyDescription("CSSCal 0.1 Farenheit Celcius")> 
+<Assembly: AssemblyCompany("by BLD Dilanga")> 
+<Assembly: AssemblyProduct("CSSCal 0.1 Farenheit Celcius")> 
+<Assembly: AssemblyCopyright("Copyright © BLD Dilanga 2010")> 
+<Assembly: AssemblyTrademark("CSSCal 0.1 Farenheit Celcius")> 
+
+<Assembly: ComVisible(False)> 
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("8a7a9fc9-b5cb-4816-95a2-d28caf9f77e0")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
+
+<Assembly: NeutralResourcesLanguageAttribute("si-LK")> 
